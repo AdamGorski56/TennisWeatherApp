c06b7b0991414de51e4eea34131cc54ba9f54854
Test <justtest@gmail.com>
2020-08-04 13:05:16 +0200

    modal change city functionality added

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ weather = new Weather('Folkestone');
 ui = new UI();
 document.addEventListener('DOMContentLoaded', getWeather);
 
+//Change location event 
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    weather.changeLocation(city);
+    getWeather();
+    $('#locModal').modal('hide');
+    document.querySelector('input').value = '';
+})  
+
+// Get and display Weather
 function getWeather(){
 weather.getWeather()
     .then(results => {
