e725fb8f68a917d5849f39537c690da9432b5604
Test <justtest@gmail.com>
2020-08-05 12:38:07 +0200

    saving last city in local storage

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-weather = new Weather('Folkestone');
-ui = new UI();
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city);
+const ui = new UI();
+
+
+
+
 document.addEventListener('DOMContentLoaded', getWeather);
 
 //Change location event 
-document.getElementById('w-change-btn').addEventListener('click', (e) => {
+document.getElementById('w-change-btn').addEventListener('click', () => {
     const city = document.getElementById('city').value;
     weather.changeLocation(city);
+    storage.setLocationData(city);
     getWeather();
     $('#locModal').modal('hide');
     document.querySelector('input').value = '';
+    
 })  
 
 // Get and display Weather
 function getWeather(){
+    
 weather.getWeather()
     .then(results => {
         ui.paint(results);
