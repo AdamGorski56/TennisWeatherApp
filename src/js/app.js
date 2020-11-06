c4c36ece5410c710dbaf14bd0002442079aaf746
Adam Gorski <adamgorski1256@gmail.com>
2020-11-06 15:43:17 +0100

    updated appearance and fixed messages v

---
+++ src/js/app.js
@@ -0,0 +1,35 @@
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city);
+const ui = new UI();
+
+
+
+
+document.addEventListener('DOMContentLoaded', getWeather);
+
+//Change location event 
+document.getElementById('w-change-btn').addEventListener('click', () => {
+    const city = document.getElementById('city').value;
+    weather.changeLocation(city);
+    storage.setLocationData(city);
+    getWeather();
+    $('#locModal').modal('hide');
+    document.querySelector('input').value = '';
+    
+})  
+
+// Get and display Weather
+function getWeather(){
+    
+weather.getWeather()
+    .then(results => {
+        ui.paint(results);
+    })
+    .catch(err => console.log(err));
+}
+
+
+
+
+
